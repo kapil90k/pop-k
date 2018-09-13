092bf03f47c6ab9028155fbcf8334a631f4f1035
Park Sanghyun <parksanghyun@Parkui-MacBook-Pro.local>
2018-09-14 02:04:41 +0900

    .

---
+++ src/main/scala/com/wikibooks/spark/ch6/scala/KafkaSample.scala
@@ -0,0 +1,36 @@
+package com.wikibooks.spark.ch6.scala
+
+import kafka.serializer.StringDecoder
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark.{SparkConf, SparkContext}
+
+// 6.2.4절 예제 6-10
+object KafkaSample {
+
+  def main(args: Array[String]) {
+    
+    import org.apache.spark.streaming.kafka._
+
+    val conf = new SparkConf()
+      .setMaster("local[*]")
+      .setAppName("KafkaSample")
+
+    val sc = new SparkContext(conf)
+    val ssc = new StreamingContext(sc, Seconds(3))
+
+    val zkQuorum = "localhost:2181"
+    val groupId = "test-consumer-group1"
+    val topics = Map("test" -> 3)
+    
+    val ds1 = KafkaUtils.createStream(ssc, zkQuorum, groupId, topics)
+    val ds2 = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](ssc, 
+        Map("metadata.broker.list" -> "localhost:9092"), 
+        Set("test"))
+
+    ds1.print
+    ds2.print
+
+    ssc.start
+    ssc.awaitTermination()
+  }
+}
